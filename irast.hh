5de91a7f55b152386bf2d3168d09183faef689d9
Oderjunkie <trickytests@gmail.com>
2021-10-03 03:06:28 +0000

    feat: Add symbol table.

--- irast.hh
+++ irast.hh
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+#include <map>
+
+namespace irast {
+        std::unordered_map<
+            std::string,                                       //  functions
+            std::unordered_map<std::string,                    //  variables
+                               std::unordered_map<std::string, //  attributes
+                                                  int>         // of variables
+                               >>
+            symbol_table;
+        // class
+} // namespace irast
